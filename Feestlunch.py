a9cebcac3ce956d892a635e39d302d30c4a2422d
Merlijnos <99071140@mydavinci.nl>
2022-09-06 12:06:39 +0100

    Feestlunch

---
+++ Feestlunch.py
@@ -0,0 +1,16 @@
+a = 0.39
+b = 17
+c = 2.78
+d = 2
+e = 0.50
+f = 3
+
+print('De prijs van de Croissants is:')
+print(a*b)
+
+print('De stokbroden kosten:')
+print(c*d)
+
+print('Je krijgt',e*f,'euro korting') 
+
+print('De eindprijs is', (c*d + a*b) - e*f)
