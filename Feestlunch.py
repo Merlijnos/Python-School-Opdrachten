28c268ee6c74c1716ef019d861f474b79c0410f5
Merlijnos <99071140@mydavinci.nl>
2022-09-06 13:52:47 +0100

    Feestlunch Format

--- Feestlunch.py
+++ Feestlunch.py
@@ -4,6 +4,7 @@ c = 2.78
 d = 2
 e = 0.50
 f = 3
+g = 10.69
 
 print('De prijs van de Croissants is:')
 print(a*b)
@@ -14,3 +15,15 @@ print(c*d)
 print('Je krijgt',e*f,'euro korting') 
 
 print('De eindprijs is', (c*d + a*b) - e*f)
+
+txt = 'De feestlunch kost je bij de bakker {} euro voor de'
+
+txt2 = '{} croissantjes en de'
+
+txt3 = '{} stokbroden als de 3 kortingsbonnen nog geldig zijn!'
+
+print(txt.format(g))
+
+print(txt2.format(b))
+
+print(txt3.format(d))
