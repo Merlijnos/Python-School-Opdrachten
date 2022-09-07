aff013c0f013e57a16a4b00812d822ff9996d083
Merlijnos <99071140@mydavinci.nl>
2022-09-07 12:05:22 +0100

    Feestlunch (input + float)

--- Feestlunch.py
+++ Feestlunch.py
@@ -1,29 +1,13 @@
-a = 0.39
-b = 17
-c = 2.78
-d = 2
-e = 0.50
-f = 3
-g = 10.69
-
-print('De prijs van de Croissants is:')
-print(a*b)
-
-print('De stokbroden kosten:')
-print(c*d)
-
-print('Je krijgt',e*f,'euro korting') 
-
-print('De eindprijs is', (c*d + a*b) - e*f)
-
-txt = 'De feestlunch kost je bij de bakker {} euro voor de'
-
-txt2 = '{} croissantjes en de'
-
-txt3 = '{} stokbroden als de 3 kortingsbonnen nog geldig zijn!'
-
-print(txt.format(g))
-
-print(txt2.format(b))
-
-print(txt3.format(d))
+print('Hoeveel mag de croissant kosten?')
+a = (input())
+print('Hoeveel croissantjes wilt u?')
+b = (input())
+print('Wat moet het stokbrood kosten?')
+c = (input())
+print('Hoeveel stokbroden wilt u?')
+d = (input())
+print('Wat zijn de kostingsbonnen waard?')
+e = (input())
+print('hoeveel kortingsbonnen heeft u?')
+f = (input())
+print('De eindprijs is', (float(a)*float(b)) + (float(c)*float(d)) - (float(e)*float(f)))
