ae7afdb62bb52e9dffae834ccbc178a2210fd3bd
Merlijnos <99071140@mydavinci.nl>
2022-09-12 13:56:54 +0100

    input en if statement

---
+++ input en if-statement.py
@@ -0,0 +1,9 @@
+print("Vul een variabele in")
+a = int(input())
+print("Vul een tweede variabele in")
+b = int(input())
+if a > b: 
+    print("a is het grootste getal " + str(a))
+else:
+    print("a is niet groter dan b")
+max = a
