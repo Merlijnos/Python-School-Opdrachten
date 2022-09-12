b4a25d3750166bb530fd5f409950bef298a96518
Merlijnos <99071140@mydavinci.nl>
2022-09-12 13:36:03 +0100

    pizzacalculator

---
+++ pizzacalculator.py
@@ -0,0 +1,23 @@
+print('Een pizza small kost u: €5')
+print('Een pizza medium kost u: €7')
+print('Een pizza large kost u: €10')
+# Hierboven staan de Prijzen van de pizzas.
+print('hoeveel small pizzas wilt u?')
+small = float(input())
+print('hoeveel medium pizzas wilt u?')
+medium = float(input())
+print('hoeveel large pizzas wilt u?')
+large = float(input())
+# Hierboven staan de vragen met een invulvak.
+smallprijs = float(5)
+mediumprijs = float(7)
+largeprijs = float(10)
+# Hierboven staan de prijzen van de pizzas.
+print('----------------------------------------')
+print('Aantal pizzas: ' 'Small:',small,'Medium:', medium,'Large:',large)
+print('Pizza small: €',small*smallprijs)
+print('Pizza medium: €',medium*mediumprijs)
+print('Pizza large: €',large*largeprijs)
+print('Totaalprijs €',(small*smallprijs)+(medium*mediumprijs)+(large*largeprijs))
+print('----------------------------------------')
+# Dit is het bonnetje van alle pizzas bij elkaar en de prijzen.
