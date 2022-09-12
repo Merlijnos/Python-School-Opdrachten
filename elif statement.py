925085e4f1968307789e5a79b38dfd1c30b6b7e3
Merlijnos <99071140@mydavinci.nl>
2022-09-12 14:55:38 +0100

    min en max

--- elif statement.py
+++ elif statement.py
@@ -3,10 +3,13 @@ a = int(input())
 print("Vul een tweede variabele in")
 b = int(input())
 if a > b: 
-    print("a is het grootste getal:" + str(a))
+    print("a is het grootste getal: " + str(a))
 elif a < b:
-    print("a is kleiner dan b:" + str(b))
+    print("a is kleiner dan b: " + str(b))
 else:
-    print('a en b zijn gelijk')
+    print("a en b zijn gelijk")
+
+print("het minimum is ", (b))
+print("het maximum is ", (a))
 max = a
 min = b
