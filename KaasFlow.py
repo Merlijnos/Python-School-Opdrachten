e078868257b355f67972d58b9d9e34059fc73908
Merlijnos <99071140@mydavinci.nl>
2022-09-13 13:16:09 +0100

    Kaasflow (geminimaliseerd)

--- KaasFlow.py
+++ KaasFlow.py
@@ -1,37 +1,37 @@
 print('Is de kaas geel?, typ 1 voor ja en 2 voor nee')
 kaas = int(input())
-nee = int(2)
-if int(nee) > int(kaas):
+nee = 2
+if nee > kaas:
   print('zitten er gaten in?')
   gaten = int(input())
-  if int(nee) > int(gaten):
+  if nee > gaten:
    print('Is de kaas belachelijk duur?')
    duur = int(input())
-   if int(nee) > int(duur):
+   if nee > duur:
          print('De kaas is Emmenthaler!')
    else: 
         print('De kaas is Leerdammer!')
   else:
    print('Is de kaas hard als steen?')
    steen = int(input())
-   if int(nee) > int(steen):
+   if nee > steen:
        print('De kaas is Parmigiano Reggiano!')
    else: 
        print('De kaas is Goudse kaas!')
 else:  
   print('heeft de kaas blauwe schimmel?')
   schimmel = int(input())
-  if int(nee) > int(schimmel):
+  if nee > schimmel:
       print('Heeft de kaas een korst?')
       korst = int(input())
-      if int(nee) > int(korst):
+      if nee > korst:
           print('De kaas is Bleu de rochbaron!')
       else:
           print("De kaas is Foume D'ambert!")
   else:
     print('Heeft de kaas een korst?')
     kaaskorst = int(input())
-    if int(nee) > int(kaaskorst):
+    if nee > kaaskorst:
         print('De kaas is Camembert!')
     else:
         print('De kaas is mozzarella!')
