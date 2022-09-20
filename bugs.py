99b17ff9aeb29dfaf03e11f682b591b193615091
Merlijnos <99071140@mydavinci.nl>
2022-09-20 12:10:47 +0200

    bugs (try exept)

--- bugs.py
+++ bugs.py
@@ -2,16 +2,16 @@ import random
 name = input('Wat is jouw naam? ')
 print('Hallo', name)
 
-favoriteSeason = input('Wat is jouw favorite seizoen' + name + '? A) Lente, B) Zomer, C) Herfst of D) Winter  ')
+favoriteSeason = input('Wat is jouw favorite seizoen ' + name + '? A) Lente, B) Zomer, C) Herfst of D) Winter  ')
 answer = favoriteSeason
 
-if favoriteSeason == 'a':
+if favoriteSeason == 'a' or favoriteSeason =='A':
    print("Ik hou ook van de lente!")
-elif favoriteSeason == 'b':
+elif favoriteSeason == 'b' or favoriteSeason =='B':
    print("De zomer is voor mij te warm.")
-elif favoriteSeason == 'c':
+elif favoriteSeason == 'c' or favoriteSeason =='C':
    print("Mooi he, al die blaadjes die dan van de boom vallen.")
-elif favoriteSeason == 'd':
+elif favoriteSeason == 'd' or favoriteSeason =='D':
    print("Is de winter niet erg koud?")
 else:
    print("Die ken ik niet...")
@@ -25,10 +25,11 @@ elif trueOrFalse == '1':
 
 num1 = random.randint(1,10)
 num2 = random.randint(5,15)
-
-number = input('En weet jij wat ' + str(num1) + '+' + str(num2) + ' is? ') 
-
-if int(number == num1 + num2):
-  print('Dat is juist')
-else:
-  print('Nee dat klopt niet '.format(name))
+try:
+   number = input('En weet jij wat ' + str(num1) + '+' + str(num2) + ' is? ') 
+   if int(number) == num1 + num2:
+      print('Dat is juist')
+   else:
+      print('Nee dat klopt niet '.format(name))
+except:
+   print('dat is geen nummer')
