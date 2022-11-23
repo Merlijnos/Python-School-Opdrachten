1773b962e92b52e84f5d5e0623d036c7f14899b4
Merlijnos <99071140@mydavinci.nl>
2022-11-23 11:05:06 +0100

    raadeens

--- raadeens.py
+++ raadeens.py
@@ -9,21 +9,26 @@ for i in range(20):
     while pogingen < 10 :
         pogingen += 1
         print('Probeer het nummer te raden!')
+        print(Getal)
         invoer = int(input())
         if invoer == Getal:
-            pogingen = 10
+            pogingen = 11
+            ronde += 1
             score += 1
             print('U heeft het getal geraden! Uw score is nu',score)
             answer = input('Wilt u nog een ronde? Ja/Nee ')
             if answer == 'nee' or answer == 'Nee':
-                sys.exit()      
+                sys.exit()         
         elif abs(Getal - int(invoer)) <= 20:
             print('U zit heel warm')
         elif abs(Getal - int(invoer)) <= 50:
-            print('U zit warm') 
+            print('U zit warm')
         elif abs(Getal - int(invoer)) >= 20:
             print('U zit koud')
-        elif abs(Getal - int(invoer)) >= 50:
-            print('U zit heel koud')
-    print('Ronde is',ronde,'Score is',score)
-    
+        if pogingen==10:
+            ronde+=1
+            score+=0
+            nognkeer=input('U heeft het getal niet geraden, wilt u nog een ronde? Ja/Nee ')
+            if nognkeer=='Nee' or nognkeer=='nee':
+                print('Ronde is',ronde,'Score is',score)    
+                sys.exit()
