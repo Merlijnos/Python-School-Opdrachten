30a44b3d1ea692a281eef447a32a29e7f16ac280
Merlijnos <99071140@mydavinci.nl>
2022-11-24 08:59:38 +0100

    raadeens

--- raadeens.py
+++ raadeens.py
@@ -5,13 +5,13 @@ for i in range(20):
     ronde = 0
     ronde += 1
     pogingen = 0
-    Getal = random.randint(1,1000)
+    getal = random.randint(1,1000)
     while pogingen < 10 :
         pogingen += 1
         print('Probeer het nummer te raden!')
-        print(Getal)
+        print(getal)
         invoer = int(input())
-        if invoer == Getal:
+        if invoer == getal:
             pogingen = 11
             ronde += 1
             score += 1
@@ -19,11 +19,11 @@ for i in range(20):
             answer = input('Wilt u nog een ronde? Ja/Nee ')
             if answer == 'nee' or answer == 'Nee':
                 sys.exit()         
-        elif abs(Getal - int(invoer)) <= 20:
+        elif abs(getal - int(invoer)) <= 20:
             print('U zit heel warm')
-        elif abs(Getal - int(invoer)) <= 50:
+        elif abs(getal - int(invoer)) <= 50:
             print('U zit warm')
-        elif abs(Getal - int(invoer)) >= 20:
+        else:
             print('U zit koud')
         if pogingen==10:
             ronde+=1
