8d40d282f47359bf140893a40f6ee57e59c6946f
Merlijnos <99071140@mydavinci.nl>
2022-12-05 09:39:09 +0100

    Boodschappenlijstje

---
+++ Boodschappenlijstje.py
@@ -0,0 +1,17 @@
+boodschappenlijst = {}
+opnieuw = True
+while opnieuw == True:
+    vrg = input('Wilt u iets toevoegen? ja/nee : ').capitalize()
+    if vrg == 'Ja':
+        product = input('Wat wilt u toevoegen : ').capitalize()
+        aantal = int(input('Hoeveel '+ product +' Wilt u? : '))
+        if product in boodschappenlijst.keys():
+            boodschappenlijst[product] += (aantal)
+        else:
+            boodschappenlijst[product] = aantal
+        print(boodschappenlijst)
+    else:
+        print('-[ BOODSCHAPPENLIJST ]-')
+        print(boodschappenlijst)
+        opnieuw = False
+        print('___________________')
